aa3fd334b2ced963d4d626d44cc56d40642d5b57
quanghien <nquanghien97@gmail.com>
2024-12-05 18:03:46 +0700

    implement: tích điểm

--- app/api/send-otp/route.ts
+++ app/api/send-otp/route.ts
@@ -6,15 +6,18 @@ export async function POST(req: NextRequest) {
   try {
     const { phone_number } = await req.json();
 
-    const accountSid = 'AC8acebcc3a49b11be6b2b6c81f81b2b8f'; 
-    const authToken = '6c0da81bd85a428aaac09fe2b050cad1';
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const serviceSid = process.env.TWILIO_SERVICE_SID;
+
+    // Thay bằng Service SID thực tế
+    if (!serviceSid) return
     const client = twilio(accountSid, authToken);
 
     const formattedPhoneNumber = phone_number.startsWith('+')
       ? phone_number
       : `+84${phone_number.slice(1)}`;
 
-    const serviceSid = 'VA5a48eb7453309efb4eb022a6a774a0cc'; // Thay bằng Service SID thực tế
     const verification = await client.verify.v2.services(serviceSid)
       .verifications
       .create({ to: formattedPhoneNumber, channel: 'sms' });
