01463576dd100d95e3082b29efbe798c7c2e6cf6
quanghien <nquanghien97@gmail.com>
2024-12-16 16:18:00 +0700

    fix FE

--- app/sieu-sao-wow-cao-chat/page.tsx
+++ app/sieu-sao-wow-cao-chat/page.tsx
@@ -137,19 +137,19 @@ async function page() {
               </div>
             </div>
           </div>
-          <p className="bg-text-blue text-[calc(1.25rem+1.5vw)] md:text-5xl py-4 font-bold text-center uppercase">Tổng giá trị lên đến 1,3 tỷ VNĐ</p>
+          <p className="bg-text-blue text-[calc(1.25rem+1.5vw)] md:text-5xl py-4 font-bold text-center uppercase">Tổng giá trị lên đến 145,000,000 VNĐ</p>
           <div className="dkdt md:w-1/2 m-auto">
             <p className="font-bold text-[calc(1.25rem+1.5vw)] text-[#84571B]">Đăng ký dùng thử</p>
           </div>
         </div>
       </section>
       <section className="mb-8">
-        <div className="max-w-5xl m-auto">
+        <div className="max-w-6xl m-auto">
           <h2 className="text-[28px] text-[#84571B] text-center uppercase">{'>'}{'>'} Tìm hiểu thêm về sản phẩm</h2>
           <div className="flex max-lg:flex-col gap-4 mb-8">
             <div className="lg:bg-[url('/trhtr.png')] lg:bg-[length:100%_100%] lg:min-h-[200px] flex flex-1 max-lg:flex-col items-center">
               <Image src="/clock.png" alt="clock" width={160} height={160} className="mr-4" />
-              <div className=" text-[#84571B] text-xl max-lg:bg-[url('/trhtr.png')] max-lg:bg-[length:100%_100%] p-8 flex justify-center items-center flex-col w-full">
+              <div className=" text-[#84571B] text-xl max-lg:bg-[url('/trhtr.png')] max-lg:bg-[length:100%_100%] p-8 flex justify-center max-md:items-center flex-col w-full text-justify">
                 <p>Thời gian tham gia từ ngày ... đến ngày ...</p>
                 <p>Thời gian công bố kết quả ...</p>
                 <p>Thời gian gửi quà sau 1 tuần kể từ ngày thông báo.</p>
@@ -157,7 +157,7 @@ async function page() {
             </div>
             <div className="lg:bg-[url('/trhtr.png')] lg:bg-[length:100%_100%] lg:min-h-[200px] flex-1 flex max-lg:flex-col justify-center items-center">
               <Image src="/group.png" alt="group" width={160} height={160} className="mr-4" />
-              <p className="text-[#84571B] text-xl max-lg:bg-[url('/trhtr.png')] max-lg:bg-[length:100%_100%] p-8 text-center">Đối tượng tham gia chương trình là các bé trong độ tuổi <strong>từ 1-16 tham gia nhảy</strong> cùng bố mẹ hoặc anh chị cô</p>
+              <p className="text-[#84571B] text-xl max-lg:bg-[url('/trhtr.png')] max-lg:bg-[length:100%_100%] p-8 max-md:text-center text-justify">Đối tượng tham gia chương trình là các bé trong độ tuổi <strong>từ 1-16 tham gia nhảy</strong> cùng bố mẹ hoặc anh chị cô</p>
             </div>
           </div>
           <div className="bg-[url('/asset_24.png')] bg-[length:100%_100%] min-h-[120px] flex items-center justify-center">
@@ -300,6 +300,18 @@ async function page() {
           </div>
         </div>
       </section>
+      <section className="flex max-md:flex-col gap-4 mb-4 max-w-5xl m-auto">
+        <div className="flex-1 flex items-center">
+          <div className="flex flex-col justify-center items-center">
+            <p className="bg-text text-[calc(1rem+1vw)] md:text-3xl max-md:text-center font-semibold">Xem video mẫu để học các điệu nhảy &quot;SIÊU SAO WOW CAO CHẤT&quot; và bấm</p>
+            <br />
+            <Link className="dkdt px-8 py-2 " href="#trial-form">Tham gia ngay</Link>
+          </div>
+        </div>
+        <div className="flex-1 px-2">
+          <LazyYouTubeEmbed videoId='6FVUCtcFDW4' />
+        </div>
+      </section>
       {/* <section className="mb-8">
         <div className="max-w-3xl m-auto">
           <div className="bg-[url('/asset_24.png')] bg-[length:100%_100%] min-h-[120px] flex items-center justify-center mb-4">
@@ -314,8 +326,8 @@ async function page() {
       </section> */}
       <section className="mb-8">
         <div className="max-w-4xl m-auto">
-          <div className="bg-[url('/asset_24.png')] bg-[length:100%_100%] min-h-[120px] flex items-center justify-center mb-4">
-            <span className="uppercase font-bold bg-text-blue text-[5vw] md:text-3xl py-8 mt-2 text-center">Danh sách<br />Người tham gia siêu sao wow cao chất</span>
+          <div className="bg-[url('/asset_24.png')] bg-[length:100%_100%] flex items-center justify-center mb-4">
+            <span className="uppercase font-bold bg-text-blue text-[4vw] md:text-3xl py-8 mt-2 text-center">Danh sách người tham gia</span>
           </div>
           <div className="flex justify-center">
             <table className="w-full">
