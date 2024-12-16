7bd90bfaf1d09d46315e948a4b1f7cde542a663c
quanghien <nquanghien97@gmail.com>
2024-12-16 14:25:02 +0700

    fix FE

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,23 @@ export default async function Home() {
 
   return (
     <main>
-      <section className="relative">
+      <section className="relative max-md:hidden">
         <div className="px-[10%] m-auto absolute top-1/2 -translate-y-1/2">
           <div className=" flex justify-between items-center">
-            <div className="w-2/3">
-              <p className="bg-text-blue uppercase font-bold text-xs lg:text-5xl lg:mb-4 py-2 lg:!leading-[64px]">
-                NHẬP KHẨU NGUYÊN LON TỪ NEWZEALAND
+            <div className="w-1/2">
+              <p className="bg-text-blue uppercase font-bold text-xs md:text-5xl lg:mb-4 py-2 lg:!leading-[64px]">
+                Giúp tăng chiều cao từ cấp độ nguyên bào
               </p>
-              <Link href="/" className="header-bg px-6 py-2 text-xs lg:text-2xl rounded-full uppercase text-white">
+              <Link href="/" className="header-bg px-6 py-2 text-xs md:text-2xl rounded-full uppercase text-white">
                 Tìm hiểu thêm
               </Link>
             </div>
           </div>
         </div>
-        <Image src="/bg-banner.webp" alt="bg-banner" width={1920} height={1080} className="w-full" />
+        <Image src="/bg-banner.png" alt="bg-banner" width={1920} height={1080} className="w-full" />
+      </section>
+      <section className="md:hidden">
+        <Image src="/bn-mobile@2x-100.jpg" alt="bn-mobile@2x-100" width={640} height={941} className="w-full" />
       </section>
       <section className="lg:mb-16">
         <div className="max-w-6xl m-auto md:px-4">
