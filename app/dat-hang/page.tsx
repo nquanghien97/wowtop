f6bdbf57fcd0528d07bd3e681b4b80af77eee80a
quanghien <nquanghien97@gmail.com>
2024-12-23 15:56:40 +0700

    fix: banner order

--- app/dat-hang/page.tsx
+++ app/dat-hang/page.tsx
@@ -24,20 +24,8 @@ async function page() {
           <CartIcon width={40} height={40} fill='white' />
         </Link>
       </div>
-      <section className="relative">
-        <div className="px-[10%] m-auto absolute top-1/2 -translate-y-1/2">
-          <div className=" flex justify-between items-center">
-            <div className="w-1/2">
-              <p className="bg-text-blue uppercase font-bold text-xs md:text-5xl lg:mb-4 py-2 lg:!leading-[64px]">
-                Giúp tăng chiều cao từ cấp độ nguyên bào
-              </p>
-              <Link href="/" className="header-bg px-6 py-2 text-xs md:text-2xl rounded-full uppercase text-white">
-                Tìm hiểu thêm
-              </Link>
-            </div>
-          </div>
-        </div>
-        <Image src="/bg-banner.png" alt="bg-banner" width={2318} height={1055} className="w-full" />
+      <section className="">
+        <Image src="/banner-order.jpg" alt="banner-order" width={2318} height={1055} className="w-full" />
       </section>
       <CongThuc />
       <section className="mb-20 bg-[url('/pt-bg-yellow.webp')] bg-[length:100%_100%]">
