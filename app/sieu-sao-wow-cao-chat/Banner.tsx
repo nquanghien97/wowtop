6fd794b6debab61cc949599af76e34612c0285f5
quanghien <nquanghien97@gmail.com>
2024-12-23 09:00:12 +0700

    fix banner]

--- app/sieu-sao-wow-cao-chat/Banner.tsx
+++ app/sieu-sao-wow-cao-chat/Banner.tsx
@@ -22,14 +22,14 @@ function Banner() {
       >
         <SwiperSlide>
           <div className="relative">
-            <Image src="/bner.png" alt="bner-dang-ky-dung-thu" width={1920} height={400} className="w-full max-h-[800px] max-lg:hidden" />
-            <Image src="/DANCE-mobile.png" alt="bner-dang-ky-dung-thu" width={1920} height={400} className="w-full lg:hidden" />
+            <Image src="/bner.png" alt="bner-dang-ky-dung-thu" width={2879} height={1308} className="w-full max-h-[800px] max-lg:hidden" />
+            <Image src="/DANCE-mobile.png" alt="bner-dang-ky-dung-thu" width={1782} height={2619} className="w-full lg:hidden" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="relative">
-            <Image src="/bner.png" alt="bner-dang-ky-dung-thu" width={1920} height={400} className="w-full max-h-[800px] max-lg:hidden" />
-            <Image src="/bn-mobile@2x-100.png" alt="bn-mobile@2x-100" width={1920} height={400} className="w-full lg:hidden" />
+            <Image src="/bner.png" alt="bner-dang-ky-dung-thu" width={2879} height={1308} className="w-full max-h-[800px] max-lg:hidden" />
+            <Image src="/bn-mobile@2x-100.png" alt="bn-mobile@2x-100" width={1782} height={2619} className="w-full lg:hidden" />
           </div>
         </SwiperSlide>
       </Swiper>
